a44e65ff80adb5f461b305592b87933224612e59
Khongchai <world1955@hotmail.com>
2020-08-12 14:16:50 +0700

    added airport and flight

--- flights/models.py
+++ flights/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
-# Create your models here.
-class Flights(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+        
+class Flight(models.Model):
+    #CASCADE = if delete airport, delete all other related data too.
+    #related_name = reverse-order relationship; from an airport, 
+    #what are the flights leaving from it.
+    #same goes for destination
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}. Duration: {self.duration}"
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Flight, Airport
 
-# Create your views here.
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "Flights": Flight.objects.all()
+    })
+
